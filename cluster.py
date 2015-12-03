f4830688265efc83844571688520162f9e8dbb46
Stephen Mayhew <swm.mayhew@gmail.com>
2015-12-03 10:57:56 -0600

    Added wals.py as a master file, updatd filesizes to have short names

--- cluster.py
+++ cluster.py
@@ -1,61 +1,16 @@
-
 import csv
-
-def mp(x):
-    """ This turns the WALS vector into an integer vector. Each feature has the format: <int> <Description>.
-    This just returns the int."""
-    out = []
-    for v in x:
-        if len(v) == 0:
-            out.append(0)
-        else:
-            out.append(int(v[0]))
-    return out
-
-PHON_INDS = (10,39)
-
-def getLangFeatures(fname, frac = 0.0, phon=False):
-    """ This returns only those languages with frac of the features non-zero."""
-    with open(fname) as csvfile:
-        f = csv.reader(csvfile, delimiter=',', quotechar='"')
-
-        i = 0
-        X = []
-        langs = []
-        for line in f:
-            if i == 0:
-                header = line
-                #print "Header len is: ", len(header)
-                i += 1
-                continue
-
-            if phon:
-                m = mp(line[slice(*PHON_INDS)])
-                sline = line[:PHON_INDS[1]]
-            else:
-                m = mp(line[10:])
-                sline = line
-
-            bools = map(lambda i: 1 if i > 0 else 0, m)
-
-            if sum(bools) > frac*len(bools):
-                X.append(m)
-                langs.append(sline)
-        return langs,X
-
+import wals
+import operator
+from sklearn.cluster import KMeans
 
 if __name__ == "__main__":
-    from sklearn.cluster import KMeans
-    langs,X = getLangFeatures("language.csv", 0.6)
+    langs,X = wals.getLangFeatures("language.csv", 0.6)
 
     km = KMeans(n_clusters = 20)
 
     km.fit(X)
     
     print km.labels_
-
-    import operator
-    
 
     with open("langclusters.txt", "w") as out:
         langlab = sorted(zip(langs, km.labels_), key=operator.itemgetter(1))
---
+++ wals.py
@@ -0,0 +1,113 @@
+import csv
+import numpy as np
+
+# Phonology index range (strict python indexing)
+PHON_INDS = (0,19)
+
+# Morphology index range (strict python indexing)
+MORPH_INDS = (19,31)
+
+
+class WALSLanguage:
+    """
+    This represents a single language in the WALS system. This contains a feature
+    vector for the WALS features.
+    """
+
+    def mp(self, x):
+        """
+        This turns the WALS vector into an integer vector. Each feature
+        has the format: <int> <Description>.
+        This just returns the int.
+        """
+        out = []
+        for v in x:
+            if len(v) == 0:
+                out.append(0)
+            else:
+                out.append(int(v[0]))
+        return out
+
+    def __init__(self, header, walslist):
+        # Is this high resource language?
+        self.hr = False
+
+        # Store everything
+        self.dctzip = zip(header, walslist)
+        self.dct = dict(self.dctzip)
+
+        # WALS feats
+        self.feats = self.mp(walslist[10:])
+
+        # lat and long
+        self.coords = (walslist[4], walslist[5])
+
+    def __getitem__(self, item):
+        return self.dct[item]
+
+    def phon_feats(self):
+        return self.feats[slice(*PHON_INDS)]
+
+    def morph_feats(self):
+        return self.feats[slice(*MORPH_INDS)]
+
+
+    def __repr__(self):
+        return "Language " + self.dct["Name"]
+
+
+def getHRLanguages(fname, hrthreshold=1000):
+    """
+    :param fname: the name of the file containing filesizes. Created using wc -l in the wikidata folder
+    :param hrthreshold: how big a set of transliteration pairs needs to be considered high resource
+    :return: a list of language names (in ISO 639-3 format?)
+    """
+
+    hrlangs = set()
+    with open(fname) as fs:
+        for line in fs:
+            sline = line.strip().split()
+            if int(sline[0]) > hrthreshold:
+                # name formats are wikidata.<langname>
+                langname = sline[1].split(".")[1]
+                hrlangs.add(langname)
+    return hrlangs
+
+
+def loadLangs(fname):
+    """
+    This loads the WALS file into the WALSLanguage data structures
+    :param fname: name of WALS file, typically language.csv
+    :return: list of WALSLanguage objects
+    """
+
+    hrlangs = getHRLanguages("filesizes.txt")
+    print hrlangs
+
+    with open(fname) as csvfile:
+        f = csv.reader(csvfile, delimiter=',', quotechar='"')
+        langs = []
+
+        header = f.next()
+        # wals features begin at index 10
+        maxvals = np.zeros(len(header[10:]))
+        for line in f:
+            lang = WALSLanguage(header, line)
+            langs.append(lang)
+            maxvals = np.maximum(maxvals, lang.feats)
+
+            if lang["iso_code"] in hrlangs:
+                lang.hr = True
+                print lang["Name"]
+
+
+        # normalize each feature by the maximum possible value.
+        for lang in langs:
+            lang.feats = lang.feats / maxvals
+
+        return langs
+
+
+if __name__ == "__main__":
+    lf = loadLangs("language.csv")
+    f = lf[8]
